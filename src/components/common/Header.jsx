48344380141ead1f76a16359795a12faf2d80f5b
muhammadega21 <dermawane988@gmail.com>
2025-06-05 11:08:46 +0700

    fix: enhance Header component layout by adding marquee for news updates

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,19 @@
 const Header = ({ title }) => {
-	return (
-		<header className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700'>
-			<div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8'>
-				<h1 className='text-2xl font-semibold text-gray-100'>{title}</h1>
-			</div>
-		</header>
-	);
+  return (
+    <header className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex gap-2 items-center">
+        <h1 className="text-2xl font-semibold text-gray-100">{title}</h1>
+        <div className="bg-gray-700 rounded-md p-2 w-full">
+          <marquee
+            behavior="scroll"
+            direction="left"
+            className="text-sm font-medium text-gray-300"
+          >
+            News: Update profile pada halaman setting
+          </marquee>
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Header;
